f8befc16d7b78513bd8262a1ebaddd1857786199
jonasnapoles <jonasnapoles@gitgub.com>
2022-03-27 03:01:18 +0100

    It is going to be uploading the alterations of the routes, where I add the routes with events and buttons to go back and forward to the history

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
         <h1>Photo gallery</h1>
+        <!-- RouterLink takes the route where it is hosted -->
+        <router-link :to="{name: 'photos', params: {id : item}}" 
+        v-for="(item, index) of arrayPhotos" :key="index">
+            <button>Photo {{item}}</button>
+        </router-link>
         <Photo />
+        <button @click="home">Home</button>
+        <button @click="previous">Previous</button>
+        <button @click="next">Next</button>
     </div>
 </template>
 
@@ -13,6 +21,22 @@ export default {
     name: 'Photos',
     components:{
         Photo
-    }
+    },
+    data(){
+        return{
+            arrayPhotos: [1, 2, 3]
+        }         
+    },
+    methods: {
+        home(){
+            this.$router.push('/')
+        },
+        previous(){
+            this.$router.go(-1)
+        },
+        next(){
+            this.$router.go(1)
+        }
+    },
 }
 </script>
